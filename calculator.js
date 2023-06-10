4d11de7b2c1566937a6aa372ee4562ebb2ab1955
Julio S <85770548+Julius-Sol@users.noreply.github.com>
2023-06-10 16:39:40 -0400

    Update variables to hold inputs of calculator

--- calculator.js
+++ calculator.js
@@ -1,7 +1,10 @@
 const display = document.querySelector(".calc-display");
 const displayText = document.querySelector(".display-text");
 
-let statement = ''
+let statement = '';
+let currentValue = '';
+let prev = '';
+let currentAction = '';
 
 //Eventlistner to know if button on calculator is being pressed. 
 //Function to handle the appropiate action for clicking a button.
@@ -14,11 +17,30 @@ calcButtonWrapper.addEventListener('click',(element)=>{
           ResetCalc();
           break;
         case 'delete':
-          statement = statement.substring(0,statement.length-1);
+          let nospace = currentValue.trim();
+          currentValue = nospace.substring(0,nospace.length-1);
           break;
-        // case 'addition':
-        //   statement = statement + '+'
-        //   break;
+        case 'addition':
+          currentAction = '+'
+          prev = currentValue;
+          currentValue = '';
+          break;
+        case 'subtract':
+          statement = statement + ' - '
+          break;
+        case 'multiply':
+          statement = statement + ' * '
+          break;
+        case 'divide':
+          statement = statement + ' / '
+          break;
+        case 'decimal':
+          if(currentValue.includes('.')){
+            break;
+          }else{
+            AppendStatement('.');
+          }
+          break;
         default:
           console.log("action")
       } 
@@ -27,15 +49,21 @@ calcButtonWrapper.addEventListener('click',(element)=>{
       AppendStatement(button.innerHTML);
     }
   }
-  console.log(statement);
+  displayText.textContent = currentValue;
+  console.log('current value: ' + currentValue);
+  console.log('prev value: ' + prev);
+  console.log('current action: ' + currentAction);
 })
 
 const AppendStatement = (value)=>{
-  statement += value;
+  currentValue += value;
 }
 
 const ResetCalc = ()=>{
-  statement = ''
+  statement = '';
+  currentValue = '';
+  prev = '';
+  prevCharacter = '';
 }
 
 
